7518d9f5a4046b7cd726a6ae06f1acda70ccdb08
fogara23 <aidan.fogarty23@mail.dcu.ie>
2022-02-01 19:18:30 +0000

    fixed linting error

--- cmd/results.go
+++ cmd/results.go
@@ -27,21 +27,6 @@ import (
 	"github.com/AidanFogarty/go-f1/pkg/flags"
 )
 
-// resultsCmd represents the results command
-var resultsCmd = &cobra.Command{
-	Use:   "results",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("results called")
-	},
-}
-
 func NewResultsCmd() *cobra.Command {
 	resultsCmd := &cobra.Command{
 		Use:   "results",
